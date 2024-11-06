7ae8c3009aa01b57d7ed2c500b0c43db5c892bc7
Noah Streller <noah.streller@gmx.ch>
2024-11-06 14:27:41 +0100

    feat: set up error tracking

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
+	import { PUBLIC_SENTRY_DSN } from '$env/static/public';
+	import * as Sentry from '@sentry/browser';
 	import '../app.css';
 	let { children } = $props();
+	Sentry.init({
+		dsn: PUBLIC_SENTRY_DSN,
+		tracesSampleRate: 0.01
+	});
 </script>
 
 {@render children()}
