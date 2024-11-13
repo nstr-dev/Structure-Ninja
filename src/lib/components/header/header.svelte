f7207a9bc6837e23625b9f1358336d8178c3f2b9
Noah Streller <noah.streller@gmx.ch>
2024-11-13 21:43:16 +0100

    fix: adjusted visibility on small devices

--- src/lib/components/header/header.svelte
+++ src/lib/components/header/header.svelte
@@ -8,7 +8,7 @@
 	class="fixed top-0 z-50 flex h-16 w-full max-w-full flex-1 items-center justify-between px-5 backdrop-blur-[20px]"
 >
 	<button
-		class="prose flex cursor-pointer items-center gap-5 opacity-80 transition-all dark:prose-invert hover:translate-x-3 hover:rotate-1 hover:opacity-100"
+		class="prose flex flex-shrink-0 cursor-pointer items-center gap-5 opacity-80 transition-all dark:prose-invert hover:translate-x-3 hover:rotate-1 hover:opacity-100"
 		on:click={() => goto('/')}
 	>
 		<img
@@ -16,11 +16,11 @@
 			alt="Structure Ninja"
 			class="m-0 mt-1 inline-block h-14 w-14 align-middle dark:invert"
 		/>
-		<h1 class="m-0 hidden whitespace-nowrap align-middle sm:inline-block">Structure Ninja</h1>
+		<h1 class="m-0 hidden whitespace-nowrap align-middle md:inline-block">Structure Ninja</h1>
 	</button>
 
-	<div class="flex items-center gap-14">
-		<div class="scale-[82%]">
+	<div class="flex items-center gap-10">
+		<div class="hidden scale-[82%] sm:block">
 			<Minimap />
 		</div>
 		<div class="flex flex-row-reverse items-center gap-3">
